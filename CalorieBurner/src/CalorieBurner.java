c06ad9a96f04cfa385cfe6e56fc651fb6e09afcb
jaylaprade <jay.l.laprade@gmail.com>
2013-09-09 14:37:58 -0700

    Added comments to CalorieBurner and TempConverter

--- CalorieBurner/src/CalorieBurner.java
+++ CalorieBurner/src/CalorieBurner.java
@@ -1,5 +1,29 @@
+/*  Homework. 2       Part 2
+ *  File Name: CalorieBurner.java
+ *  @Author: Jason Laprade
+ *  Date: September 9th, 2013
+ *  
+ *  Problem Statement: Measure the amount of energy burned in calories using METs and 
+ *  the weight of the person
+ *  
+ *  Overall Plan
+ *  1) Establish METs for the three activities
+ *  2) Greet the user
+ *  3) Ask for the weight in pounds.
+ *  4) Get the number of hours used for each activity
+ *  5) Convert the weight from pounds to kilos
+ *  6) Calculate the calories burned based on the formula:
+ *  	Calories/minute = 0.0.175 X MET X Weight in kilos
+ *  7) Display calories
+ *  
+ *  Classes needed and purpose: 'java.util' will be needed for the general purpose i/o from terminal
+ */
+
+
 import java.util.Scanner;
 public class CalorieBurner {
+	
+	// declare our constants for activities to METS
 	
 	public static final int RUNNING_METS = 10;
 	public static final int BBALLING_METS = 8;
@@ -9,14 +33,23 @@ public class CalorieBurner {
 	public static void main(String[] args)
 	{
 	
+		// declare a double for the result of Total METS
+		
 		double totalMets;
 		
+		// Display a title and ask for input
 		
 		System.out.println("\t Calorie Burner \n \n");
 		System.out.println("Enter the weight you wish to calculate in pounds:");
 		
+		// create an instance of the new Scanner Class from the user
 		Scanner keyboard = new Scanner(System.in);
+		
+		// assign first number to the weight
+		
 		double weightOfPerson = keyboard.nextDouble();
+		
+		// Get METS for each of the activities
 		
 		System.out.println("\nEnter the number of hours spent running:");
 		double hoursRunning = keyboard.nextDouble();
@@ -27,12 +60,18 @@ public class CalorieBurner {
 		System.out.println("\nEnter the number of hours spent sleeping:");
 		double hoursSleeping = keyboard.nextDouble();
 		
+		
+		// Use the formula kilos = pounds/2.2
 		weightOfPerson = weightOfPerson/2.2; //Change weight to kilos
 		
+		
+		// Calculate the totalMets
 		totalMets = ((hoursRunning * RUNNING_METS) + (hoursBBalling * BBALLING_METS) + (hoursSleeping * SLEEPING_METS));
 		
+		// Get the caloriesBurned
 		double caloriesBurned = (0.0175 * totalMets * weightOfPerson) * 60;
 		
+		// Display the information to the user
 		System.out.println("Based on the weight entered, " + caloriesBurned + " calories were burned");
 		
 		keyboard.close();
--- TemperatureConverter/src/TemperatureConverter.java
+++ TemperatureConverter/src/TemperatureConverter.java
@@ -1,3 +1,20 @@
+/*  Homework. 2       Part 3
+ *  File Name: TemperatureConverter.java
+ *  @Author: Jason Laprade
+ *  Date: September 9th, 2013
+ *  
+ *  Problem Statement: Write a program that converts degrees Fahrenheit to Celsius using the formula
+ *  
+ *  degreesC= 5(degreesF - 32)/9
+ *  
+ *  Overall Plan
+ *  1) Greet the user
+ *  2) Get the temperature in Fahrenheit
+ *  3) Calculate out using the formal
+ *  4) Display results
+ 
+ *  Classes needed and purpose: 'java.util' will be needed for the general purpose i/o from terminal
+ */
 import java.util.Scanner;
 
 public class TemperatureConverter {
@@ -6,11 +23,13 @@ public class TemperatureConverter {
 	{
 
 		Scanner keyboard = new Scanner(System.in);
-		
+
+		// Display a title and ask for the temperature
+		System.out.println("\t Temperature Converter \n \n");
 		System.out.println("Please enter the Fahrenheit temperature you wish to convert:");
 		int tempToConvert = keyboard.nextInt();
 		
-		
+		//Display the conversion and finish
 		System.out.printf( tempToConvert + " degrees Fahrenheit is %.1f degrees Celsius.", 5 * (tempToConvert - 32.0)/9.0);
 		
 		keyboard.close();
