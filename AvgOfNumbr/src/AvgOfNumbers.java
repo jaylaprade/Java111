8da4ea04a41ed617b8c0c3d9702feb2b8000cb36
jaylaprade <jay.l.laprade@gmail.com>
2013-09-09 15:43:34 -0700

    Added Comments to avgofnum

--- AvgOfNumbr/src/AvgOfNumbers.java
+++ AvgOfNumbr/src/AvgOfNumbers.java
@@ -1,3 +1,18 @@
+/*  Homework. 2       Part 1
+ *  File Name: AvgOfNumbr.java
+ *  @Author: Jason Laprade
+ *  Date: September 9th, 2013
+ *  
+ *  Problem Statement: Read in three whole numbers and output the average of the three.
+ *  
+ *  Overall Plan
+ *  1) Greet the user
+ *  2) Ask for three numbers
+ *  3) Get Average of the three numbers
+ *  4) Display results to user
+ *  
+ *  Classes needed and purpose: 'java.util' will be needed for the general purpose i/o from terminal
+ */
 
 import java.util.Scanner;
 
@@ -8,8 +23,13 @@ public class AvgOfNumbers {
 		{
 			Scanner keyboard = new Scanner(System.in);
 			
+			// Display a title and describe function
+			
+			System.out.println("\t Average of Three integers \n \n");
 			System.out.println("This program will give you the average ");
-			System.out.println("of three numbers:");
+			System.out.println("of three whole numbers.");
+			
+			//Ask for the numbers
 			
 			System.out.println("Enter your first number:");
 			int firstNumber = keyboard.nextInt();
@@ -20,8 +40,10 @@ public class AvgOfNumbers {
 			System.out.println("Enter your third number:");
 			int thirdNumber = keyboard.nextInt();
 			
+			//Calculate the Average
 			int avgOfThree = (firstNumber + secondNumber + thirdNumber) / 3;
 			
+			//Display results to user
 			System.out.println("The Average of your three numbers = "
 												+ avgOfThree);
 		}
