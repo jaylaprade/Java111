c90a0bc8a8eab74829d9177a6331ddaf8b1af663
jaylaprade <jay.l.laprade@gmail.com>
2013-09-15 17:36:46 -0700

    Add TextLineReverse Homework

---
+++ TextLineReverse/src/TextLineReverse.java
@@ -0,0 +1,36 @@
+/*  Homework. 3       Part 1
+ *  File Name: MortgagePaymentCalc.java
+ *  @Author: Jason Laprade
+ *  Date: September 12, 2013
+ *  
+ *  Problem Statement: Write a program that will read in a line of text as input and then output the
+ *  line with the line with the first word moved to the end of the line.
+ *  
+ *  Overall Plan
+ *  1) Greet user
+ *  2) Get a line of text
+ *  3) Reverse 
+ *  4) output monthly interest
+ *  5) Ask for the monthly payment amount
+ *  6) Out the amount of the payment of principle
+ *  
+ *  Classes needed and purpose: 'javax.swing.JOptionPane' will be needed for the general purpose windows
+ */
+
+import java.text.NumberFormat;
+
+import javax.swing.JOptionPane;
+
+
+public class TextLineReverse {
+	
+	public static void main(String[] args)
+	{
+
+		
+		
+		
+		
+	}
+	
+}
